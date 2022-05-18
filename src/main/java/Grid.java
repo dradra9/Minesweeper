e85375876475ad2fd47a43a77d03208455027ff7
Melvin <melvinjoy10@gmail.com>
2022-05-18 23:03:23 +0100

    Update 2

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Grid {
 
     //Attributes
@@ -6,6 +8,7 @@ public class Grid {
     int numberOfColumns;
     int numberOfMines;
     Tiles [][] newGrid;
+    String [][] displayArray;
     int difficulty;
 
     //Constructor
@@ -15,20 +18,29 @@ public class Grid {
         this.numberOfRows = numberOfRows;
         this.numberOfColumns = numberOfColumns;
         this.numberOfMines = numberOfRows;
-        this.newGrid = Tiles [numberOfRows][numberOfColumns];
+        this.newGrid = new Tiles [numberOfRows][numberOfColumns];
+
+        for (int i = 0; i < newGrid.length; ++i) {
+            for(int j = 0; j < newGrid[i].length; ++j) {
+
+                newGrid[i][j] = new Tiles(false,0,false);
+            }
+        }
 
 
-        this.numberOfRows = 10;
-        this.numberOfColumns = 10;
-        this.numberOfMines  = 10;
 
+        for( int i = 0; i < this.numberOfMines; i++ ){
+
+            int random_rows = (int)Math.floor(Math.random()*(this.numberOfRows));
+            int random_column = (int)Math.floor(Math.random()*(this.numberOfColumns));
+
+            this.newGrid [random_rows][random_column].haveMine = true;
+
+        }
 
     }
 
     //Methods
-
-
-
 
 
    /* public Tiles[][] getNewGrid() {
@@ -47,11 +59,74 @@ public class Grid {
         return newGrid;
     }    */
 
+    public void startGame(){
+
+        System.out.print("    ");
+
+
+
+        Scanner userInput = new Scanner(System.in);
+
+
+
+        String displayArray[][] = new String[this.numberOfRows][this.numberOfColumns];
+
+        System.out.print("    ");
+        for(int i = 0; i<this.numberOfRows; i++){
+
+            System.out.print( (i+1) + "  ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < displayArray.length; i++) {
+            System.out.print((i+1) + "  ");
+
+            for (int j = 0; j < displayArray[i].length; j++) {
+
+                displayArray[i][j] = " _ ";
+                System.out.print(displayArray[i][j]);
+
+            }
+            System.out.println("\n");
+
+            //
+
+        }
+
+
+
+
+    }
+
     public int getNumberOfColumns() {
+
+
         return numberOfColumns;
+    }
+
+    public void setNumberOfColumns(int numberOfColumns) {
+
+        this.numberOfColumns = numberOfColumns;
     }
 
     public int getNumberOfRows() {
         return numberOfRows;
+    }
+
+    public void setNumberOfRows(int numberOfRows) {
+        this.numberOfRows = numberOfRows;
+    }
+
+    public int getNumberOfMines() {
+        return numberOfMines;
+    }
+
+    public void setNumberOfMines(int numberOfMines) {
+        this.numberOfMines = numberOfMines;
+    }
+
+    public Tiles[][] getNewGrid() {
+
+        return newGrid;
     }
 }
